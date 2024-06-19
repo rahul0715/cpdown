9a6a9929893da8bc4a6166159ac7ddeb6e694406
rahul0715 <ajaychouhan26367@gmail.com>
2024-06-20 01:51:10 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,7 @@
 api_id = 25586552
 api_hash = "f265cba9d76dc6ad70914accbe758f47"
-bot_token = "6700392192:AAFVtSrOBmNkx-9be-0oiMBdzJpO5AxrCYs"
-auth_users = [0]
+bot_token = "6891484332:AAHAiVZDQZc7CHW8SRYg_iVe-rC3e20_E2w"
+auth_users = [5881684718]
 sudo_user = 0
 log_channel = -1002038285084
 txt_channel = -1002038285084
